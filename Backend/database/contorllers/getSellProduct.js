3d8cb2e179365b9c8dfab02f4acb28357dbbd920
Akash kumar sahu <sahuakashkumar915@gmail.com>
2025-06-25 23:26:36 +0530

    added the updated fetures to system

--- Backend/database/contorllers/getSellProduct.js
+++ Backend/database/contorllers/getSellProduct.js
@@ -2,10 +2,13 @@ import sellProduct from "../models/sellsData.model.js";
  export const getSellProduct =async (req,res)=>{
     try {
       const sellitems=  await sellProduct.find();
-      const formattedSales = sellitems.map((sale, index) => ({
-        id: String(index + 1), // or use sale._id.toString() if needed
+      const formattedSales = sellitems.map((sale,index) => ({
+        // id:sale.id, //this return the mongodb _id
+        id: String(index + 1), 
+        // or use sale._id.toString() if needed
         customer: sale.customer,
-        date: sale.date ? sale.date.toISOString() : new Date().toISOString(), // Ensure date is in ISO format
+        date: sale.date,
+        // date: sale.date ? sale.date.toISOString() : new Date().toISOString(), // Ensure date is in ISO format
         items: sale.items,
         subtotal: sale.subtotal,
         tax: sale.tax,
